403ae59295980d7a1023541bc4a548365ce6acda
Carl J. Conrad <carlj.conrad@gmail.com>
2020-01-20 14:28:20 -0800

    html js routes updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workoutdb", { useNewUrlParser: true });
 
 
 // const databaseUrl = "workoutDB";
