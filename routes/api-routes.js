c11ccb018976560fda05471859b837eb96923ea4
Carl J. Conrad <carlj.conrad@gmail.com>
2020-01-24 01:24:21 -0800

    working version 1.0

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,9 @@
 const db = require("../models");
 
+
 module.exports = function (app) {
+    let exerciseArr =[];
+
     app.get("/api/workouts", (req, res) => {
         console.log("get api/workouts")
         // console.log(res);
@@ -15,30 +18,36 @@ module.exports = function (app) {
 
     });
 
-    app.post("/api/workouts", (req, res) => {
-        console.log("post that workout!");
-        console.log("body before create");
-        console.log(req);
-        var newExercise = req.body;
-        db.Workout.create({})
-            .then(newWorkout => {
-                console.log("response after create!");
-                console.log(newWorkout)
-                db.Workout.findByIdAndUpdate(newWorkout._id, { $push: { exercises: newExercise } }, { new: true })
-                    .then(dbWorkout => {
-                        res.json(dbWorkout);
-                    }).catch(err => {
-                        res.status(400).json(err);
+    // app.post("/api/workouts", (req, res) => {
+        // console.log("post that workout!");
+        // console.log("body before create");
+        // console.log(req.body);
+        // exerciseArr.push(req.body);
+        // console.log(exerciseArr);
+        // var newExercise = req.body;
+        // db.Workout.create({})
+        //     .then(newWorkout => {
+        //         // console.log("response after create!");
+        //         // console.log(newWorkout)
+        //         db.Workout.findByIdAndUpdate(newWorkout._id, { $push: { exercises: newExercise } }, { new: true })
+        //             .then(dbWorkout => {
+        //                 res.json(dbWorkout);
+        //             }).catch(err => {
+        //                 res.json(err);
 
-                    })
-            });
-    });
+        //             })
+        //     })
+        //     .catch(err => {
+        //         res.json(err);
+        //     })
+    // });
 
     app.put("/api/workouts/:id", (req, res) => {
-        // console.log("put it with an id");
+        console.log("put it with an id");
         // console.log(req.body);
         var addExercise = req.body;
         var id = req.params.id;
+        exerciseArr.push(addExercise);
         // console.log(addExercise);
         // console.log(req.params)
         db.Workout.findByIdAndUpdate(id, { $push: { exercises: addExercise } }, { new: true })
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = function(app) {
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/index.html"));
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/../public/index.html"));
+// });
 
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/stats.html"));
@@ -14,8 +14,8 @@ app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/exercise.html"));
 });
 
-// app.get("/exercise?", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/../public/exercise.html"));
-// });
+// app.get("/api/workouts", (req, res) => {
+
+// })
 
 };
